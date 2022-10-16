ad63c40d9c9ddcd5cfd06b72b31da5de429fb344
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-10-16 11:09:33 -0500

    ⚡Added write notices to files functionality

--- scraper.py
+++ scraper.py
@@ -1,13 +1,47 @@
+import os
+import datetime
 import requests
 import lxml.html as html
 
 
 HOME_URL = 'https://www.larepublica.co/'
 
-XPATH_LINK_TO_ARTICLE = '//h2/a/@href'
+XPATH_LINK_TO_ARTICLE = '//text-fill[not(@class)]/a/@href'
 XPATH_TITLE = '//div[@class="mb-auto"]/h2/span/text()'
 XPATH_SUMMARY = '//div[@class="lead"]/p/text()'
 XPATH_BODY = '//div[@class="html-content"]/p/text()'
+
+
+def parse_notice(link: str, today):
+    if not (len(link) > 0):
+        return
+    try:
+        response = requests.get(link)
+        print("link: ", link)
+        if response.status_code == 200:
+            notice = response.content.decode('utf-8')
+            parsed = html.fromstring(notice)
+            try:
+                title = parsed.xpath(XPATH_TITLE)[0]
+                title = title.replace('\"', '').lstrip()
+                summary = parsed.xpath(XPATH_SUMMARY)[0]
+                body = parsed.xpath(XPATH_BODY)
+            except IndexError as e:
+                print(f"Index: {e}")
+                return
+            with open(f'{today}/{title}.txt', 'w', encoding='utf-8') as f:
+                f.write(title)
+                f.write('\n\n')
+                f.write(summary)
+                f.write('\n\n')
+                for p in body:
+                    f.write(p)
+                    f.write('\n')
+
+        else:
+            raise ValueError(f'Error : {response.status_code}')
+    except Exception as e:
+        print(e)
 
 
 def parse_home():
@@ -17,9 +51,13 @@ def parse_home():
             home = response.content.decode('utf-8')
             parsed = html.fromstring(home)
             links = parsed.xpath(XPATH_LINK_TO_ARTICLE)
-            # title = parsed.xpath(XPATH_TITLE)[0]
-            # summary = parsed.xpath(XPATH_SUMMARY)[0]
-            # body = parsed.xpath(XPATH_BODY)
+            today = datetime.date.today().strftime('%d-%m-%Y')
+
+            if not os.path.isdir(today):
+                os.mkdir(today)
+
+            for link in links:
+                parse_notice(link, today)
         else:
             raise ValueError(f'Error : {response.status_code}')
     except Exception as e:
