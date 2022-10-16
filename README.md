074920540d65eae4ebaf09d8f0e54d219a5ec124
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-10-16 10:04:09 -0500

    🚀 Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## La Republica Scraper
+<br>
+<img src="./images/larepublica.jpeg">
+<br>
+
+A python app for web scraping with Xpath. It extracts info for the site [larepublica.co](https://www.larepublica.co/).
---
+++ scraper.py
@@ -0,0 +1,20 @@
+import requests
+import lxml.html as html
+
+
+HOME_URL = "https://www.larepublica.co/"
+
+XPATH_LINK_TO_ARTICLE = "//h2/a/@href"
+XPATH_BODY = ""
+
+
+def parse_home():
+    ...
+
+
+def run():
+    ...
+
+
+if __name__ == "__main__":
+    run()
